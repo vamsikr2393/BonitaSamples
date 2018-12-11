efc150c3ffe9910d14fe35c5e61e7972c880881f
Vamsi Krishna Koorapati <vkoorapati@evoketechnologies.com>
2018-12-11 16:13:25 +0530

    initial commit

---
+++ Basic.js
@@ -0,0 +1,13 @@
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.logg('Hello Evoke');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+{
+    console.log(message);
+}
---
+++ Basic.ts
@@ -0,0 +1,10 @@
+class Startup {
+    public static main(): number {
+        console.logg('Hello Evoke');
+        return 0;
+    }
+}
+
+Startup.main(); {
+    console.log(message);
+}
